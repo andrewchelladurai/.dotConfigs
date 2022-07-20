ee8da638039dad5e29c22339099ca34e46aa3597
Andrew Chelladurai <theunknownandrew@gmail.com>
2022-07-20 16:12:26 +0530

    Add new packages to remove before first upgrade

--- nix-setup-arch.sh
+++ nix-setup-arch.sh
@@ -19,7 +19,7 @@ ln -s ~/..bash_profile ~/.config/bash/..bash_profile
 source ~/.bashrc
 
 # Remove all the unnecesary packages from the fresh-install
-sudo pacman -Rs thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman htop firewalld firefox vi eos-quickstart welcome eos-log-tool eos-apps-info meld reflector reflector-simple stoken openconnect xed mpv eos-update-notifier
+sudo pacman -Rs thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman htop firewalld firefox vi eos-quickstart welcome eos-log-tool eos-apps-info meld reflector reflector-simple stoken openconnect xed mpv eos-update-notifier eos-rankmirrors file-roller endeavouros-xfce4-terminal-colors endeavouros-theming lxappearance-gtk3 yad-eos
 
 # Now do a dist-upgrade
 upgradesys
