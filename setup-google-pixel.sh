34ac72ecb3317d5ae849571d2f6585ed9684d01f
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2022-08-20 00:47:22 +0530

    Include app links and comment irrelevant ones.

--- setup-google-pixel.sh
+++ setup-google-pixel.sh
@@ -4,17 +4,16 @@
 #
 
 # Give Greenify special rights to do it's magic
-# Restart Greenify once for the rights to take effect
+# https://play.google.com/store/apps/details?id=com.oasisfeng.greenify
 adb shell pm grant com.oasisfeng.greenify android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.oasisfeng.greenify android.permission.GET_APP_OPS_STATS
 adb shell pm grant com.oasisfeng.greenify android.permission.READ_LOGS
 adb shell pm grant com.oasisfeng.greenify android.permission.DUMP
+# Restart Greenify once for the rights to take effect
 adb shell am force-stop com.oasisfeng.greenify
 
-###########################
 # Give Tasker the rights it deserves and needs
-###########################
-
+# https://play.google.com/store/apps/details?id=net.dinglisch.android.taskerm
 # Check running services (Context - Application)
 adb shell pm grant net.dinglisch.android.taskerm android.permission.DUMP
 # Read System Logs (Context - Event - LogCat Entry)
@@ -22,10 +21,8 @@ adb shell pm grant net.dinglisch.android.taskerm android.permission.READ_LOGS
 # Write Secure Settings (Action - Settings - Custom Setting)
 adb shell pm grant net.dinglisch.android.taskerm android.permission.WRITE_SECURE_SETTINGS
 
-###########################
 # Give AutoApps the rights it deserves and needs
-###########################
-
+# https://play.google.com/store/apps/details?id=com.joaomgcd.autoappshub
 # LogCat for AutoTools
 adb shell pm grant com.joaomgcd.autotools android.permission.READ_LOGS
 # Secure Settings for AutoTools
@@ -37,16 +34,15 @@ adb shell pm grant com.joaomgcd.autoinput android.permission.WRITE_SECURE_SETTIN
 # Secure Settings for AutoWear
 adb shell pm grant com.joaomgcd.autowear android.permission.WRITE_SECURE_SETTINGS
 
-###########################
 # Give Join Clipboard Permission on Android 10
-# https://joaoapps.com/AutoApps/Help/Info/com.joaomgcd.join/android_10_read_logs.html
-###########################
+# https://play.google.com/store/apps/details?id=com.joaomgcd.join
 adb -d shell appops set com.joaomgcd.join SYSTEM_ALERT_WINDOW allow
 adb shell pm grant com.joaomgcd.join android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.joaomgcd.join android.permission.READ_LOGS
 adb shell am force-stop com.joaomgcd.join
 
-###########################
 # Make XDA Navigation Gestures useful
-###########################
-adb shell pm grant com.xda.nobar android.permission.WRITE_SECURE_SETTINGS
+# https://play.google.com/store/apps/details?id=com.xda.nobar
+# This is no longer applicable since the advent of inbuilt gesture navigation.
+# adb shell pm grant com.xda.nobar android.permission.WRITE_SECURE_SETTINGS
+
