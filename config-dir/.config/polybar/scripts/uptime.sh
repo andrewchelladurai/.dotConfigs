b6fdb98280f6b9d937646656090a0028b9fdcf29
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2023-02-04 12:25:53 +0530

    Remove unnecessary config files

--- config-dir/.config/polybar/scripts/uptime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "﯁ " $(uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ weeks\?,/w/' |
-    sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/')
-
