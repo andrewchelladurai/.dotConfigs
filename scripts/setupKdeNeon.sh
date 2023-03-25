3603d2a2f2def31c127f42d068a91c353182fd27
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2023-03-25 16:26:59 +0530

    Update setupKdeNeon.sh

--- scripts/setupKdeNeon.sh
+++ scripts/setupKdeNeon.sh
@@ -3,19 +3,25 @@
 # Preferred Distro : KDE Neon : https://neon.kde.org/
 #
 
-# Download the dotFiles repo 
-mkdir ~/.dotConfigs/ && cd ~/.dotConfigs/
-git clone https://github.com/andrewchelladurai/.dotConfigs.git
+# Install necessary initial packages
+sudo apt install stow git
+
+# Download the dotFiles repo & switch to it
+git clone https://github.com/andrewchelladurai/.dotConfigs-KDE-Neon.git
+cd .dotConfigs-KDE-Neon/
+
+# Simulate the stowing of the packages and remove the conflicts
+# When all good, restow without the simulation flag [-n]
 stow -nvR home-dir/ config-dir/
-# Validate that the links are all present
-ls -la ~/ ~/.config/ | grep -i '^l' | tr -s " " | cut -d" " -f 9-11
 
 # Reload the modifications or open a terminal to load the new .bashrc file
 source ~/.bashrc
 
-# Make links to actual content directories in a separate partition
-# Required only if the data partition is on a separate partition from OS mount-point
+# Remove unnecessary folders
 rmdir ~/Documents/ ~/Downloads/ ~/Pictures/ ~/Videos/ ~/Music/
+
+# Required only if the data partition is on a separate partition from OS mount-point
+# Make links to actual content directories in a separate partition
 ln -s /media/$(whoami)/Data/Documents/ Documents
 ln -s /media/$(whoami)/Data/Downloads/ Downloads
 ln -s /media/$(whoami)/Data/Volatile/ Volatile
@@ -23,33 +29,15 @@ ln -s /media/$(whoami)/Media/Music/ Music
 ln -s /media/$(whoami)/Media/Pictures/ Pictures
 ln -s /media/$(whoami)/Media/Movies/ Videos
 
-# Download required Nerd fonts :
-# https://www.nerdfonts.com/font-downloads
-# https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts
-# and place it in ~/.local/share/fonts/
-# 
-# DejaVuSansMono
-# FiraCode
-# JetBrainsMono
-# Noto
-# SourceCodePro
-#
-# Remove the unecessary font families from the extracts
-find . -type f -iname \*Windows*ttf -delete
-find . -type f -iname \*Mono.ttf -delete
-find . -type f -iname \*Semi*ttf -delete
-find . -type f -iname \*Extra*ttf -delete
-find . -type f -iname \*Black*ttf -delete
-find . -type f -iname \*Light*ttf -delete
-find . -type f -iname \*Medium*ttf -delete
-find . -type f -iname \*Thin*ttf -delete
-find . -type f -iname \*otf -delete
-find . -type f -iname \*Retina*ttf -delete
-find . -type f -iname \*Ext*ttf -delete
-rm Noto*Emoji*ttf Noto*Condensed*ttf Noto*Display*ttf
+# Create preferred folder tree structure 
+mkdir -p ~/Documents/Media/{Audio,Video,Images}
+mkdir -p ~/Documents/Projects/{Mobile,Web,Desktop,Api}
+mkdir -p ~/Documents/Projects/Mobile/{Android,iOS}
+mkdir -p ~/.tools/sdk/{android,flutter,dart,go}
+mkdir -p ~/.local/share/{themes,icons}
 
 # Remove all the unnecesary packages from the fresh-install
-TO-DO
+sudo apt purge plasma-discover-common plasma-vault plasma-wallpapers-addons plasma-welcome plasma-firewall plasma-runners-addons plasma-widgets-addons ark firefox khelpcenter kwalletmanageri vim-common
 
 # Now do a dist-upgrade
 upgradesys
@@ -68,9 +56,6 @@ wget -O - http://repo.sinew.in/keys/enpass-linux.key | sudo apt-key add -
 # Download and Install Unified Remote Server
 wget https://www.unifiedremote.com/download/linux-x64-deb && sudo dpkg --install linux-x64-deb
 
-# Download and install Nordvpn
-wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb && sudo dpkg -i nordvpn*.deb
-
 # Install Syncthing : https://syncthing.net/downloads/
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
@@ -78,23 +63,21 @@ curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 # Download and install the latest release of ActivityWatch Timetracker
 https://github.com/ActivityWatch/activitywatch/releases
 
-# Install the bashtop system monitor : https://github.com/aristocratos/bashtop | https://packages.azlux.fr/
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-# alternate PPA : sudo add-apt-repository ppa:bashtop-monitor/bashtop && sudo apt install bashtop
-
-
 # For Android AVD emulation performance, installl KVM packages for Linux
 # https://developer.android.com/studio/run/emulator-acceleration?utm_source=android-studio#vm-linux
 sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 sudo adduser $(whoami) kvm
 
 # Install necessary apps
-sudo apt install -y ktorrent kdiff3 kubuntu-restricted-extras git vlc virtualbox virtualbox-guest-additions-iso kget sqlite filelight gimp google-chrome-stable enpass inkscape digikam xserver-xorg-input-synaptics nordvpn wireguard neovim default-jdk bashtop kate
+sudo apt install -y kubuntu-restricted-extras git vlc sqlite filelight gimp google-chrome-stable enpass inkscape digikam wireguard neovim default-jdk
 sudo apt-get install -y --no-install-recommends libreoffice
 
 # Install proprietary drivers
 sudo ubuntu-drivers autoinstall
+
+# Set locale to use 24H time-format
+# https://unix.stackexchange.com/questions/553679/set-clock-to-24-hour-format-for-all-users
+sudo localectl set-locale C.UTF-8
 
 # This is for a Wifi Card Bug on HP Pavilion Notebook AB219TX (N8L68PA#ACJ)
 # have'nt observred WiFi drops for quite some time
