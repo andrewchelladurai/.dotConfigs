cb4dfd400d20eff70b3b5a778f28b69b260aa4c3
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2023-12-21 11:10:29 +0530

    Add file

--- scripts/setup-Kde-Neon.sh
+++ scripts/setup-Kde-Neon.sh
@@ -18,7 +18,10 @@ stow -nvR home-dir/ config-dir/
 source ~/.bashrc
 
 # Remove unnecessary folders
-rmdir ~/Documents/ ~/Downloads/ ~/Pictures/ ~/Videos/ ~/Music/
+rm -rf ~/Documents/ ~/Downloads/ ~/Pictures/ ~/Videos/ ~/Music/
+
+# Create preferred folder tree structure
+mkdir -p ~/Documents/{Others,Media/{Audio,Video,Images},Projects,Tools/{lazygit,sdk/{android,flutter,dart,go}}}
 
 # Required only if the data partition is on a separate partition from OS mount-point
 # Make links to actual content directories in a separate partition
@@ -29,49 +32,49 @@ ln -s /media/$(whoami)/Media/Music/ Music
 ln -s /media/$(whoami)/Media/Pictures/ Pictures
 ln -s /media/$(whoami)/Media/Movies/ Videos
 
-# Create preferred folder tree structure 
-mkdir -p ~/Documents/Media/{Audio,Video,Images}
-mkdir -p ~/Documents/Projects/{Mobile,Web,Desktop,Api}
-mkdir -p ~/Documents/Projects/Mobile/{Android,iOS}
-mkdir -p ~/Tools/sdk/{android,flutter,dart,go}
-mkdir -p ~/Tools/astudio
-mkdir -p ~/.local/share/{themes,icons}
+# For capability to add PPA repositories
+sudo apt install -y software-properties-common software-properties-qt
 
-# Remove all the unnecesary packages from the fresh-install
-sudo apt purge plasma-discover-common plasma-vault plasma-wallpapers-addons plasma-welcome plasma-firewall plasma-runners-addons plasma-widgets-addons ark firefox khelpcenter kwalletmanageri vim-common
+# Install proprietary drivers
+sudo ubuntu-drivers autoinstall
+
+# Remove all the unnecessary packages from the fresh-install
+sudo apt purge -y plasma-discover-common plasma-vault plasma-wallpapers-addons plasma-welcome plasma-firewall plasma-runners-addons plasma-widgets-addons ark firefox khelpcenter kwalletmanager vim-common kate vlc
 
 # Now do a dist-upgrade
 upgradesys
 
-# For capability to add PPA repositories
-sudo apt install -y software-properties-common software-properties-qt
+# Install Google Chrome
+# https://support.google.com/chrome/answer/95346
+cd ~/Documents/Others/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && rm ./google-chrome-stable_current_amd64.deb && cd ~/
 
-# PPA : Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-
-# PPA : Enpass.io (Password manager)
-sudo sh -c 'echo "deb http://repo.sinew.in/ stable main" >> /etc/apt/sources.list.d/enpass.list'
-wget -O - http://repo.sinew.in/keys/enpass-linux.key | sudo apt-key add -
+# Install Enpass.io (Password manager)
+# https://support.enpass.io/app/getting_started/installing_enpass.htm
+echo "deb https://apt.enpass.io/  stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
 
 # Download and Install Unified Remote Server
-wget https://www.unifiedremote.com/download/linux-x64-deb && sudo dpkg --install linux-x64-deb
+# https://www.unifiedremote.com/download/other#linux
+cd ~/Documents/Others/ && wget -c https://www.unifiedremote.com/download/linux-x64-deb && sudo dpkg -i ./linux-x64-deb && rm ./linux-x64-deb && cd ~/
 
-# Install Syncthing : https://syncthing.net/downloads/
+# Install Syncthing
+# https://syncthing.net/downloads/
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 
 # Download and install the latest release of ActivityWatch Timetracker
 https://github.com/ActivityWatch/activitywatch/releases
 
-# Install Lazygit : https://github.com/jesseduffield/lazygit
-mkdir -p ~/Tools/lazygit/ && cd ~/Tools/lazygit/
+# Install Lazygit
+# https://github.com/jesseduffield/lazygit#ubuntu
+cd ~/Documents/Tools/lazygit/
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar -xvf lazygit.tar.gz && cd
+wget -c lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xvf lazygit.tar.gz lazygit && sudo install lazygit /usr/local/bin && rm lazygit.tar.gz && cd ~/
 
-# Install GitHub CLI : https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-cd Downloads/
+# Install GitHub CLI
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+cd ~/Documents/Others/
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -80,28 +83,27 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt install gh -y
 
 # For Android AVD emulation performance, installl KVM packages for Linux
-# https://developer.android.com/studio/run/emulator-acceleration?utm_source=android-studio#vm-linux
+# https://developer.android.com/studio/run/emulator-acceleration#vm-linux
 sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 sudo adduser $(whoami) kvm
 
 # Install necessary packages for Flutter Development
-# https://docs.flutter.dev/get-started/install/linux
-sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
+# https://docs.flutter.dev/get-started/install/linux#linux-prerequisites
+sudo apt-get install -y clang cmake git ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev
 
 # Install necessary apps
-sudo apt install -y kubuntu-restricted-extras git vlc sqlite filelight gimp google-chrome-stable enpass inkscape digikam wireguard neovim default-jdk
+sudo apt install -y kubuntu-restricted-extras git sqlite filelight gimp google-chrome-stable enpass inkscape digikam wireguard neovim default-jdk krita kompare elisa ktouch kontact zanshin arianna kommit
+
 sudo apt-get install -y --no-install-recommends libreoffice
-
-# Install packages necessary for Flutter
-# https://docs.flutter.dev/get-started/install/linux
-sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
-
-# Install proprietary drivers
-sudo ubuntu-drivers autoinstall
 
 # Set locale to use 24H time-format
 # https://unix.stackexchange.com/questions/553679/set-clock-to-24-hour-format-for-all-users
 sudo localectl set-locale C.UTF-8
+
+# Remove unnecessary wallpapers
+cd /usr/share/wallpapers/ && \
+sudo rm -rf ./Canopee/ ./Cascade/ ./Cluster/ ./BytheWater/ ./ColdRipple/ ./ColorfulCups/ ./DarkestHour/ ./Elarun/ ./EveningGlow/ ./FallenLeaf/ ./Flow/ ./FlyingKonqui/ ./Grey/ ./Honeywave/ ./IceCold/ ./Kay/ ./Kite/ ./Kokkini/ ./Opal/ ./PastelHills/ ./summer_1am/ ./Shell/ ./Volna/ && \
+cd ~\
 
 # This is for a Wifi Card Bug on HP Pavilion Notebook AB219TX (N8L68PA#ACJ)
 # have'nt observred WiFi drops for quite some time
