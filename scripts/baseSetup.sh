a90ec3428c81455bcc3419cd1f7150fe6bb3a695
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2024-01-10 10:08:14 +0530

    Update multiple blocks to move to EndeavourOS

--- scripts/baseSetup.sh
+++ scripts/baseSetup.sh
@@ -1,10 +1,10 @@
 #
-# For Ubuntu Based Distros (KDE Specific)
-# Preferred Distro : KDE Neon : https://neon.kde.org/
+# For [Arch Linux] (https://archlinux.org/) Based Distros (KDE Specific)
+# Preferred Distro : [Endeavour OS] (https://endeavouros.com/)
 #
 
 # Install necessary initial packages
-sudo apt install stow git
+sudo pacman -S stow git
 
 # Download the dotFiles repo & switch to it
 cd ~/ && \
@@ -26,7 +26,7 @@ source ~/.bashrc
 rm -rf ~/Documents/ ~/Downloads/ ~/Pictures/ ~/Videos/ ~/Music/
 
 # Create preferred folder tree structure
-mkdir -p ~/Documents/{Others,Media/{Audio,Video,Images},Projects,Tools/{lazygit,sdk/{android,flutter,dart,go}}}
+mkdir -p ~/Documents/{Others,Media/{Audio,Video,Images},Projects,Tools/sdk/{android,flutter,dart,go}}
 
 # Required only if the data partition is on a separate partition from OS mount-point
 # Make links to actual content directories in a separate partition
@@ -41,62 +41,26 @@ ln -s /media/$(whoami)/Media/Movies/ Videos
 ## Remove & Install Packages
 ##
 
-# For capability to add PPA repositories
-sudo apt install -y software-properties-common software-properties-qt
-
-# Install proprietary drivers
-sudo ubuntu-drivers autoinstall
+## Install Nvidia Drivers for current Laptop
+sudo pacman -S nvidia-inst && \
+nvidia-inst && \
+sudo pacman -Rns nvidia-inst
 
 # Remove all the unnecessary packages from the fresh-install
-sudo apt purge -y plasma-discover-common plasma-vault plasma-wallpapers-addons plasma-welcome plasma-firewall plasma-runners-addons plasma-widgets-addons ark firefox khelpcenter kwalletmanager vim-common kate vlc
+sudo pacman -Rns stoken meld haruna mpv ark file-roller firewalld glances nano-syntax-highlighting openconnect networkmanager-openconnect inxi plasma-disks kdeplasma-addons firefox vi kate hwinfo
+# Remove EndeavourOS Packages
+sudo pacman -Rns endeavouros-theming eos-plasma-sddm-config eos-qogir-icons eos-settings-plasma eos-apps-info eos-log-tool eos-quickstart reflector reflector-simple welcome eos-bash-shared eos-rankmirrors eos-update-notifier
+# Remove package BUT NOT the depedencies
+sudo pacman -Rdd v4l-utils qt5-tools
 
 # Perform a system upgrade.
 upgradesys
 
-# Install necessary apps
-sudo apt install -y kubuntu-restricted-extras git sqlite filelight krita google-chrome-stable enpass digikam wireguard neovim default-jdk elisa dragonplayer btop gh
-sudo apt-get install -y --no-install-recommends libreoffice
+# Install necessary apps from main repo
+sudo pacman -Syu git sqlite filelight krita digikam neovim elisa btop github-cli dragon
 
-##
-## Install Packages NOT in default repos
-##
-
-# Install Google Chrome
-# https://support.google.com/chrome/answer/95346
-cd ~/Documents/Others/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && rm ./google-chrome-stable_current_amd64.deb && cd ~/
-
-# Install Enpass.io (Password manager)
-# https://support.enpass.io/app/getting_started/installing_enpass.htm
-echo "deb https://apt.enpass.io/  stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
-wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
-
-# Download and Install Unified Remote Server
-# https://www.unifiedremote.com/download/other#linux
-cd ~/Documents/Others/ && wget -c https://www.unifiedremote.com/download/linux-x64-deb && sudo dpkg -i ./linux-x64-deb && rm ./linux-x64-deb && cd ~/
-
-# Install Syncthing
-# https://syncthing.net/downloads/
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-
-# Download and install the latest release of ActivityWatch Timetracker
-https://github.com/ActivityWatch/activitywatch/releases
-
-# Install Lazygit
-# https://github.com/jesseduffield/lazygit#ubuntu
-cd ~/Documents/Tools/lazygit/
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -c lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xvf lazygit.tar.gz lazygit && sudo install lazygit /usr/local/bin && rm lazygit.tar.gz && cd ~/
-
-# Install GitHub CLI
-# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-cd ~/Documents/Others/
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update
+# Install necessary apps from AUR repo
+yay -Syu google-chrome enpass-bin lazygit syncthing unified-remote-server
 
 ##
 ## Install necessary packages for Dev work.
